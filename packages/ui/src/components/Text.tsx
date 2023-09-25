4baccb561873b45181350a513f8da60f071cf8e8
brayn003 <brayn003@gmail.com>
2023-09-26 02:29:06 +0530

    chore: added workspace and simple build tooling

---
+++ packages/ui/src/components/Text.tsx
@@ -0,0 +1,7 @@
+function Text({children}: {children: any}) {
+    return (
+        <p>{children}</p>
+    )
+}
+
+export default Text;
---
+++ packages/ui/src/views/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Text from '../components/Text';
+
+function Main () {
+    return <Text>Hello World!</Text>
+}
+
+export default Main;
