976968241779f4772c354d29575fbec4860bfe4d
prashantpaddune <padadunep@gmail.com>
2023-09-26 23:33:35 +0530

    cloud-ide: App structure and clean up
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- packages/cloud-ide/src/index.tsx
+++ packages/cloud-ide/src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Main } from '@quik/ui';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <Main />
+      <App />
   </React.StrictMode>
 );
 
